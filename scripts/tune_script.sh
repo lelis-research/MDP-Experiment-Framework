111960e9ab71c8302214817c7ecca64353037bcc
KIARASH AGHAKASIRI <aghakasi@narval2.narval.calcul.quebec>
2025-02-28 11:38:36 -0500

    gitignore

--- scripts/tune_script.sh
+++ scripts/tune_script.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem=1G        # memory per node
 #SBATCH --time=0-10:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
-#SBATCH --account=<rrg-lelis>
+#SBATCH --account=def-lelis
 #SBATCH --mail-user=aghakasi@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH --array=0-1
+#SBATCH --array=0-0
 
 source ~/MDP-Experiment-Framework/venvs/rl_v1/bin/activate
 python tune_hp.py --env MiniGrid-ChainEnv-v0 --agent A2C_v1 --num_episodes 200 
